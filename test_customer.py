8ad755681138e38ca6f891e55575bf08b6e34bc5
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2021-10-20 11:21:36 -0500

    Finished all tests for get_wallet_coin method in Customer class.

--- test_customer.py
+++ test_customer.py
@@ -2,8 +2,6 @@ import unittest
 from customer import Customer
 
 class TestgetWalletcoin(unittest.TestCase):
-    """Tests for the get_wallet_coin method in Customer class"""
-
     def setUp(self):
         self.customer = Customer()
 
@@ -12,6 +10,25 @@ class TestgetWalletcoin(unittest.TestCase):
         returned_coin = self.customer.get_wallet_coin("Quarter")
         self.assertEqual(.25,returned_coin.value)
 
+    def test_dime_string_returns_dime_instance(self):
+        """Testing that passing in 'Dime' returns dime object"""
+        returned_coin = self.customer.get_wallet_coin("Dime")
+        self.assertEqual(.10,returned_coin.value)
+
+    def test_nickel_string_returns_nickel_instance(self):
+        """Testing that passing in 'Nickel' returns nickel object"""
+        returned_coin = self.customer.get_wallet_coin("Nickel")
+        self.assertEqual(.05,returned_coin.value)
+
+    def test_penny_string_returns_penny_instance(self):
+        """Testing that passing in 'Penny' returns penny object"""
+        returned_coin = self.customer.get_wallet_coin("Penny")
+        self.assertEqual(.01,returned_coin.value)
+
+    def test_not_valid_string_returns_none_instance(self):
+        """Testing that passing in a non-valid string returns None"""
+        returned_coin = self.customer.get_wallet_coin("Loonie")
+        self.assertEqual(None,returned_coin)
 
 if __name__ == "__main__":
     unittest.main()
